f21d3a9ce2b4d042c0d8f52be16b0c17ec837638
Eyob Tariku <eyobtariku@gmail.com>
2020-03-23 15:29:01 +0300

    Add `Supplier` model to the shared app

--- shared/models.py
+++ shared/models.py
@@ -2,15 +2,16 @@ import uuid
 
 from django.db import models
 
+from django_countries.fields import CountryField
+
 
 class Region(models.Model):
     """
-    Regional states.
+    Product buyer regions.
     """
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     name = models.CharField(max_length=120)
-    code = models.CharField(max_length=3, unique=True)
-    amharic_name = models.CharField(max_length=255)
+    code = models.CharField(max_length=4, unique=True)
 
     class Meta:
         verbose_name = 'Regional State'
@@ -32,7 +33,6 @@ class Location(models.Model):
         related_name='locations'
     )
     name = models.CharField(max_length=120)
-    amharic_name = models.CharField(max_length=255)
 
     class Meta:
         order_with_respect_to = 'region'
@@ -52,10 +52,55 @@ class Union(models.Model):
         related_name='unions'
     )
     name = models.CharField(max_length=120)
-    amharic_name = models.CharField(max_length=255)
 
     class Meta:
         order_with_respect_to = 'region'
 
     def __str__(self):
         return self.name
+
+
+class ProductCategory(models.Model):
+    """
+    Category of products available for purchase.
+    """
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    name = models.CharField(max_length=120)
+
+    class Meta:
+        ordering = ('name', )
+        verbose_name = 'Product Category'
+        verbose_name_plural = 'Product Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    category = models.ForeignKey(
+        ProductCategory,
+        on_delete=models.PROTECT,
+        related_name='products'
+    )
+    name = models.CharField(max_length=120)
+    description = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ('name', )
+
+    def __str__(self):
+        return self.name
+
+
+class Supplier(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    company_name = models.CharField(max_length=60)
+    city = models.CharField(max_length=60)
+    country = CountryField()
+
+    class Meta:
+        ordering = ('company_name', )
+
+    def __str__(self):
+        return self.company_name
