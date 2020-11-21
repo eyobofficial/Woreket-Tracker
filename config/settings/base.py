12b605c5b353e8231dab8c1751990d07da03108c
Eyob Tariku <eyobtariku@gmail.com>
2020-11-21 20:52:52 +0300

    Install & setup `djangorestframework-camel-case` package

--- config/settings/base.py
+++ config/settings/base.py
@@ -202,3 +202,19 @@ FIXTURES = ['categories', 'customers', 'units', 'ports', 'roles']
 
 # Environment
 ENVIRONMENT = config('ENVIRONMENT')
+
+
+# DRF
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+        'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
+        # Any other renders
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'djangorestframework_camel_case.parser.CamelCaseFormParser',
+        'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
+        'djangorestframework_camel_case.parser.CamelCaseJSONParser',
+        # Any other parsers
+    ),
+}
